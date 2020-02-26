0a90753b75f12815a073e779e9c5cb52f9776706
Iaroslav <yaroslavposkryakov@gmail.com>
2020-02-26 17:14:59 +0300

    Add files via upload

---
+++ SP/SP_8/code/lab08/cmake-build-debug/CMakeFiles/lab08.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab08.dir/mlispgen.cpp.obj"
+  "lab08.pdb"
+  "lab08.exe"
+  "lab08.exe.manifest"
+  "liblab08.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
